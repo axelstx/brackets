31e554b1a69adee6698729551097d8bc8bc9fb4b
axelstx <stx0789@gmail.com>
2020-10-12 15:33:55 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  let obj = new Map();
+  bracketsConfig.forEach(element => obj.set(element[0], element[1]));
+
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+
+    if (arr[arr.length - 1] === current) {
+      arr.pop();
+    } else if (obj.has(current)) {
+      arr.push(obj.get(current));
+    } else {
+      return false;
+    }
+  }
+
+  return arr.length === 0;
 }
